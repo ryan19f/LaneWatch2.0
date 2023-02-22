af91bd9695f22bc20b94c24a2c59109e92bb7126
ryan19f <41567699+ryan19f@users.noreply.github.com>
2023-02-22 14:21:09 +0400

    Add files via upload

---
+++ lanes-vid.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+def display_lines(image, lines):
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return image
+
+def region_of_interest(image):
+    height, width = image.shape[:2]
+    left_top = (int(width * 0.45), int(height * 0.6))
+    left_bottom = (int(width * 0.1), int(height))
+    right_top = (int(width * 0.55), int(height * 0.6))
+    right_bottom = (int(width * 0.9), int(height))
+    vertices = np.array([[left_bottom, left_top, right_top, right_bottom]], dtype=np.int32)
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, vertices, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+cap = cv2.VideoCapture("solidYellowLeft.mp4")
+while True:
+    _, image = cap.read()
+    lane_image = image.copy()
+    canny_image = canny(lane_image)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    lane_lines = display_lines(lane_image, lines)
+    cv2.imshow("Lane Tracking", lane_lines)
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
